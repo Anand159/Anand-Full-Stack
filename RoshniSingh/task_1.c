328c98f9aa5779cc1685949e5767bb78f8aa896e
Roshnihtce <roshnihtce87@gmail.com>
2024-06-15 17:09:30 +0530

    task

---
+++ RoshniSingh/task_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("Please enter any Number");
+    scanf("%d",&number);
+    int sum=0;
+    for(int i=1; i<=number;i++);
+    {
+        sum+=0;
+    }
+    printf("Total count is : %d",number);
+    return 0;
+
+}
