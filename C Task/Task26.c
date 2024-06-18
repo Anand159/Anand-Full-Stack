6664160833bfa572b365efb22614a07639bcac9b
Anand159 <anandkumarsoni154@gmail.com>
2024-06-18 13:07:37 +0530

    escape Number

---
+++ C Task/Task26.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    for(int i=1;i<=10;i++)
+    {
+        if(i==5 || i==8)
+        {
+
+        }
+        else
+        {
+            printf("%d ", i);
+        }
+
+    }
+
+
+
+
+    return 0;
+}
