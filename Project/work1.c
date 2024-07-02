621681dc34fcdb93e40e22f85a97ebf7db1167a1
Anand159 <anandkumarsoni154@gmail.com>
2024-07-02 21:52:11 +0530

    Project_1

--- Project/work1.c
+++ Project/work1.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 
 int main()
- {
+{
     char operator;
     int FirstNumber, SecondNumber;
 
-    while (1) 
+    while (1)
     {
-        
+
         printf("Enter an operator (+, -, *, /), if want to exit press x: ");
         scanf(" %c", &operator);
 
-        if (operator == 'x' || operator == 'X') 
+        if (operator== 'x' || operator== 'X')
         {
             break;
         }
         printf("Enter two Number: ");
         scanf("%d %d", &FirstNumber, &SecondNumber);
-        switch (operator) 
+        switch (operator)
         {
-            case '+':
-                printf("%d + %d = %d\n", FirstNumber, SecondNumber, FirstNumber + SecondNumber);
-                break;
-            case '-':
-                printf("%d - %d = %d\n", FirstNumber, SecondNumber, FirstNumber - SecondNumber);
-                break;
-            case '*':
-                printf("%d * %d = %d\n", FirstNumber, SecondNumber, FirstNumber * SecondNumber);
-                break;
-            case '/':
-                printf("%d / %d = %d\n",FirstNumber,SecondNumber,FirstNumber/SecondNumber);
-                break;
-            default:
-                printf("Invalid operator.\n");
+        case '+':
+            printf("%d + %d = %d\n", FirstNumber, SecondNumber, FirstNumber + SecondNumber);
+            break;
+        case '-':
+            printf("%d - %d = %d\n", FirstNumber, SecondNumber, FirstNumber - SecondNumber);
+            break;
+        case '*':
+            printf("%d * %d = %d\n", FirstNumber, SecondNumber, FirstNumber * SecondNumber);
+            break;
+        case '/':
+            printf("%d / %d = %d\n", FirstNumber, SecondNumber, FirstNumber / SecondNumber);
+            break;
+        default:
+            printf("Invalid operator.\n");
         }
     }
     return 0;
